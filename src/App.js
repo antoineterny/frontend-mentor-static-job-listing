6d446863ab4d23d110bafb48124f3cfcd54bad71
antoineterny <63339704+antoineterny@users.noreply.github.com>
2021-02-01 22:17:54 +0100

    la logique fonctionne

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
+import React, { useState } from "react"
 import "./App.scss"
-import data from './data.json'
-import JobDisplay from './components/JobDisplay'
+import data from "./data.json"
+import JobDisplay from "./components/JobDisplay"
 
 function App() {
   const [jobs, setJobs] = useState(data)
   return (
     <div className="App">
       {jobs.map(job => (
-        <JobDisplay job={job}/>
+        <JobDisplay job={job} key={job.id} jobs={jobs} setJobs={setJobs} />
       ))}
     </div>
   )
--- src/components/JobDisplay.jsx
+++ src/components/JobDisplay.jsx
@@ -1,16 +1,61 @@
 import React from "react"
+import "./JobDisplay.scss"
 
-const JobDisplay = ({ job }) => (
+const JobDisplay = ({ job, jobs, setJobs }) => (
   <div className="JobDisplay" key={job.id}>
-    <h2>{job.company}</h2>
-    <h1>{job.position}</h1>
-    <p>
-      {job.postedAt}
-      <span>•</span>
-      {job.contract}
-      <span>•</span>
-      {job.location}
-    </p>
+    <img src={job.logo} alt="" />
+    <div className="description">
+      <h5>
+        {job.company} {job.new && <span className="new">new</span>}{" "}
+        {job.featured && <span className="featured">featured</span>}
+      </h5>
+      <h4>{job.position}</h4>
+      <p>
+        {job.postedAt}
+        <span>•</span>
+        {job.contract}
+        <span>•</span>
+        {job.location}
+      </p>
+    </div>
+    <div className="tablets">
+      <div
+        className="tablet"
+        onClick={() => {
+          setJobs(jobs.filter(j => j.role.includes(job.role)))
+        }}
+      >
+        {job.role}
+      </div>
+      <div
+        className="tablet"
+        onClick={() => {
+          setJobs(jobs.filter(j => j.level.includes(job.level)))
+        }}
+      >
+        {job.level}
+      </div>
+      {job.languages.map(lang => (
+        <div
+          className="filter-tablet"
+          onClick={() => {
+            setJobs(jobs.filter(j => j.languages.includes(lang)))
+          }}
+        >
+          {lang}
+        </div>
+      ))}
+      {job.tools.map(tool => (
+        <div
+          className="filter-tablet"
+          onClick={() => {
+            setJobs(jobs.filter(j => j.tools.includes(tool)))
+          }}
+        >
+          {tool}
+        </div>
+      ))}
+    </div>
   </div>
 )
 
